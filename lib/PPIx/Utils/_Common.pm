b27593a74d7c5028f072514ccaa68ed3d8644abd
Dan Book <grinnz@grinnz.com>
2025-05-04 05:14:37 -0400

    Bump version

--- lib/PPIx/Utils/_Common.pm
+++ lib/PPIx/Utils/_Common.pm
@@ -6,7 +6,7 @@ use warnings;
 use Exporter 'import';
 use Scalar::Util 'blessed';
 
-our $VERSION = '0.004';
+our $VERSION = '0.005';
 
 our @EXPORT_OK = qw(
     is_ppi_expression_or_generic_statement
